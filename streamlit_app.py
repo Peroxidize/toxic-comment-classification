fd2713c3d192d3da0e60b3e1425d5fd9736266dd
Peroxidize <marionemmanuelbuhat@gmail.com>
2024-04-30 23:51:50 +0800

    added vocabulary.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,41 @@
 import streamlit as st
 import numpy as np
-import tensorflow
+import re
+import pickle
+import tensorflow as tf
 import pandas as pd
-from keras.layers import TextVectorization
-from keras.saving import load_model
 
-model = load_model('toxic-comment-classification.keras')
+model = tf.keras.models.load_model('toxic-comment-classification.keras')
 
-df = pd.read_csv("train.csv", sep=",", header = 0)
-list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-list_sentences = df["comment_text"]
-
-max_tokens = 25000
-max_len = 100
-vectorize = TextVectorization(
-    max_tokens=max_tokens,
+vectorize = tf.keras.layers.TextVectorization(
+    max_tokens=25000,
     output_mode='int',
-    output_sequence_length=max_len,
-    standardize="lower_and_strip_punctuation",
+    output_sequence_length=100,
 )
-vectorize.adapt(list_sentences)
+vectorize.load_assets(".")
+
+list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
 st.title("Toxic Comment Classification")
 input_text = st.text_area("Enter the comment")
+temp_text = ""
 
 if st.button("Predict"):
     if input_text:
-        vector_input_text = vectorize(np.array([input_text]))
-        result = model.predict(vector_input_text)
-        st.write("The comment is")
-        for index, percentage in enumerate(result[0]):
-            st.write(f"{list_classes[index]}: {percentage*100:.2f}%")
+        temp_text = input_text
+        try:
+            cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', input_text)
+            vector_input_text = vectorize(np.array([cleaned_text]))
+            result = model.predict(vector_input_text)
+            st.write("The comment is")
+            for index, percentage in enumerate(result[0]):
+                st.write(f"{list_classes[index]}: {percentage*100:.2f}%")
+        except:
+            cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', temp_text)
+            vector_input_text = vectorize(np.array([cleaned_text]))
+            result = model.predict(vector_input_text)
+            st.write("The comment is")
+            for index, percentage in enumerate(result[0]):
+                st.write(f"{list_classes[index]}: {percentage*100:.2f}%")
     else:
         st.write("Please enter a comment.")
